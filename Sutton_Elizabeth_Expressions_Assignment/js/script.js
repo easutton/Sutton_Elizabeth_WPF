07b9789f004fb27562af2c69d16f1af8afdcc049
Elizabeth Sutton <easutton@fullsail.edu>
2015-04-15 16:31:58 -0400

    Initial code for calculator

--- Sutton_Elizabeth_Expressions_Assignment/js/script.js
+++ Sutton_Elizabeth_Expressions_Assignment/js/script.js
@@ -1,1 +1,11 @@
 //Elizabeth Sutton, 4/12/15, Expression Assignment
+
+//Calculate your home's price per square foot
+
+//Givens: overall square footage of home, square footage of nonliving spaces, price paid for home
+
+var overallFootage = prompt("In order to calculate the price per square foot, please enter the total square footage of your home:");
+var livingSpace = prompt("Enter the square footage of the nonliving spaces:");
+var pricePaid = prompt("Enter the amount you paid for your home:")
+var priceSF = (overallFootage - livingSpace) / pricePaid;
+alert("The price of your home per square foot is " + priceSF);

df62073b8bdac2b2ee76a3bd6ab3c1830b2d16a5
Elizabeth Sutton <easutton@fullsail.edu>
2015-04-16 10:27:39 -0400

    Tried adding array
    Tried adding array

--- Sutton_Elizabeth_Expressions_Assignment/js/script.js
+++ Sutton_Elizabeth_Expressions_Assignment/js/script.js
@@ -1,4 +1,9 @@
-//Elizabeth Sutton, 4/12/15, Expression Assignment
+/*
+Elizabeth Sutton
+SDI Section 2
+Expression Assignment
+4/12/15
+ */
 
 //Calculate your home's price per square foot
 
@@ -9,3 +14,29 @@ var nonLiving = prompt("Enter the square footage of the nonliving spaces:");
 var pricePaid = prompt("Enter the amount you paid for your home:")
 var priceSF = pricePaid / (overallFootage - nonLiving);
 alert("The price of your home per square foot is " + priceSF);
+console.log
+
+//include an array
+var arr = ["Helpful", "Not helpful"];
+var question = prompt("Did you find this calculator " + arr[0] + " or " arr[1] + "?");
+
+
+
+
+
+
+/*
+DON'T FORGET TO ADD TEST VALUES AT END, COMMENTED IN!!!
+i.e. "I typed in 5 for length and 6 for height and my calculator gave me 30 for the area."
+
+DON'T FORGET, YOU MUST MAKE AT LEAST 5 COMMITS!!
+
+"Hard coded" means to type in a specific value.
+
+Number(ageNow) < casts the var as a digit number, not canatonizing text
+OR
+parseInt(ageNow) which determines that the first character of ageNow is a digit and treats the whole
+thing like a number
+
+
+ */

293e12522626079b18fd190ce3668d2423f4af50
Elizabeth Sutton <easutton@fullsail.edu>
2015-04-23 15:50:07 -0400

    Code changes

--- Sutton_Elizabeth_Conditionals_Assignment/js/script.js
+++ Sutton_Elizabeth_Conditionals_Assignment/js/script.js
@@ -5,21 +5,52 @@
  4/23/15
  */
 
-/*How much will your monthly payment be on your new car?
-    prompt: new or used?
-    prompt: make and model?
-    prompt: cost of vehicle?
-*/
+/*You found a car you want to buy. How much will the monthly payments be? Can you afford it?
+  1. Based on year of car, determine if it's considered new or used (determines loan rate and term)
+  2. Determine monthly payment based on cost, loan rate and term
+  3. Determine if you can afford this payment based on your monthly allowance and income
 
-var loanAmt;
-var loanTerm = choose 36, 48 or 60 months
-var intRate = 6%;
 
+ prompt: how much does car cost? (given)
+ prompt: what is the year, make and model?
+ prompt: what is your monthly allowance to spend on a car? (given)
+ prompt: what is your monthly income? (given)
+ calculate:
+ 1. Monthly payment of car
+ 2. Car payment vs. budget
+ */
+
+
+//1. Determine if car is new or used
+//TERNARY OPERATOR
+var age = prompt("So you want to buy a car. First, let's determine if it's considered new or used.\n\nPlease enter the age of the vehicle in years:");
+var car;
+
+
+
+
+//2. Determine monthly payment
 //ARITHMETIC OPERATORS
 /*loan amount X intRate = interest
-interest + loanAmt = totalLoan
-totalLoan / loanTerm = monthly payment
-*/
+ interest + loanAmt = totalLoan
+ totalLoan / loanTerm = monthly payment
+ */
+
+var carCost = prompt("How much does the car cost?\nDo not use commas or decimals. Use whole numbers only.");
+var allowance = prompt("How much do you want to spend on a car payment each month?");
+
+var used;
+var usedCarRate = 1.08;
+var newCarRate = 1.03;
+var usedCarTerm = 36;
+var newCarTerm = 60;
+
+
+/*
+(carCost * carRate) = totalLoan
+totalLoan / carTerm = monthlyPayment
+allowance - monthlyPayment = if True, you can buy the car; if False, you cannot afford the car.
+ */
 
 
 //CONDITIONAL STATEMENTS
@@ -36,14 +67,7 @@ if(used) {
     newCarTerm();
 }
 
-//TERNARY OPERATOR
-var age = 4;
-var car;
-
-//If the car is more than 5 years old, then it's a used car.
-car = (age > 5) ? "used car" : "new car";
-console.log(car);
-
+console.log();
 
 
 //LOGICAL OPERATOR   Can I afford the monthly payment for this car?
@@ -57,3 +81,10 @@ if(monthlyPayment < allowance && income > allowance){
 }else{
     console.log("Sorry, you cannot afford this car.");
 }
+
+
+
+
+
+
+

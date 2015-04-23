1cd5bf07b0259de626029e78778953660f285f94
Elizabeth Sutton <easutton@fullsail.edu>
2015-04-23 12:19:44 -0400

    Finally getting an idea to work

--- Sutton_Elizabeth_Conditionals_Assignment/js/script.js
+++ Sutton_Elizabeth_Conditionals_Assignment/js/script.js
@@ -5,38 +5,6 @@
  4/23/15
  */
 
-/*
-What generation do you belong to according to your age? (Baby Boomer, Gen X, Gen Y, Gen Z)
-babyBoomer = 1946-1964
-genX = 1965-1983
-genY = 1984-2000
-genZ = 2000+
-
-prompt: enter your age
-
-2015 - yourAge = birthYear
-"You are Gen X"
-
-if greater than 1946 and less than 1965, then you are a babyBoomer
-
-
-
-//before below, add calculator to determine year born
-
-var year = 1963;
-var generation;
-var babyBoomer;
-var genX;
-var genY;
-var genZ;
-
-//If greater than 1946 and less than 1965, then you are a babyBoomer
-generation = (year >= 1946 + year <=1964) ? "Baby Boomer" : ""
-
- */
-
-
-
 /*How much will your monthly payment be on your new car?
     prompt: new or used?
     prompt: make and model?
@@ -47,8 +15,30 @@ var loanAmt;
 var loanTerm = choose 36, 48 or 60 months
 var intRate = 6%;
 
+//Arithmetic Operators
 /*loan amount X intRate = interest
 interest + loanAmt = totalLoan
 totalLoan / loanTerm = monthly payment
 */
 
+//Conditional Statements
+if(used) {
+    usedCarRate();
+}else{
+    newCarRate();
+}
+
+
+if(used) {
+    usedCarTerm();
+}else{
+    newCarTerm();
+}
+
+//Ternary Operator
+var age = 4;
+var car;
+
+//If the car is more than 5 years old, then it's a used car.
+car = (age > 5) ? "used car" : "new car";
+console.log(car);

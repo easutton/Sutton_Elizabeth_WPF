9cf243440984bd145c7ed7f44e70a296c1eb6866
Elizabeth Sutton <easutton@fullsail.edu>
2015-04-30 11:09:45 -0400

    Circumference code

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -7,5 +7,10 @@ SDI Section 02
 //Circumference: Calculate the circumference of a circle
 
 function calculateCircumference(){
-    
+    var radius = 1; //radius
+    var Pi = 3.14159; //known Pi
+    var circumf = 2(Pi*Radius); //calculation
+    console.log("The circumference of the circle is " + circumf + "."); //text for output
 }
+
+calculateCircumference(); //output solution

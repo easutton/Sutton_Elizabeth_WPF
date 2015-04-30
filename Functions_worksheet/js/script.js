dec5a6a188395f1d843c8287e4417bafb1038098
Elizabeth Sutton <easutton@fullsail.edu>
2015-04-30 11:18:21 -0400

    Circumference code corrections

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -7,10 +7,17 @@ SDI Section 02
 //Circumference: Calculate the circumference of a circle
 
 function calculateCircumference(){
-    var radius = 1; //radius
+    var radius = 3; //radius
     var Pi = 3.14159; //known Pi
-    var circumf = 2(Pi*Radius); //calculation
+    var circumf = 2*Pi*radius; //calculation
     console.log("The circumference of the circle is " + circumf + "."); //text for output
 }
 
 calculateCircumference(); //output solution
+
+/*Testing
+My initial test used 1 as the radius, however my code was wrong and it presented a minor error due to wrong casing.
+I fixed it and it output "The circumference of the circle is 6.28318."
+
+I tested it again using 3 as the radius and got: "The circumference of the circle is 18.849539999999998."
+ */
